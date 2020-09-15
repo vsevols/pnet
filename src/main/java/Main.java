90e5041a22eda89fc6ba015cbd88bdccb2ead8ea
Vlad <vsevols@gmail.com>
2020-09-15 11:38:54 +0300

    Compiled

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,24 @@
 import it.tdlight.tdlight.Client;
+import it.tdlight.tdlight.Init;
+import it.tdlight.tdlight.Log;
+import it.tdlight.tdlight.utils.CantLoadLibrary;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CantLoadLibrary {
+        // Initialize TDLight native libraries
+        Init.start();
+
+        // Set TDLib log level to 1
+        Log.setVerbosityLevel(1);
+
+        // Uncomment this line to print TDLib logs to a file
+        // Log.setFilePath("logs" + File.separatorChar + "tdlib.log");
+
         Client client = new Client();
+
+        // Initialize the TDLib client
+        client.initializeClient();
+
+        // Now you can use the client
     }
 }
