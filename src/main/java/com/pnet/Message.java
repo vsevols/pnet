d36c8c54cd14481f23861bb78ba4d14bd6debe05
Vlad <vsevols@gmail.com>
2020-09-17 01:27:25 +0300

    ...

--- src/main/java/com/pnet/Message.java
+++ src/main/java/com/pnet/Message.java
@@ -3,13 +3,18 @@ package com.pnet;
 import it.tdlight.tdlib.TdApi;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * TODO: persp: rename to PNetTdMessage, extract class Message
+ */
 @RequiredArgsConstructor
-public class Message extends TdApi.Message {
+public class Message {
     public final PartyId phone;
     public final String text;
-    Message(TdApi.Message msg){
+    public final TdApi.Message nativeMessage;
+    Message(TdApi.Message nativeMessage){
         phone=null;
-        TdApi.MessageText text = (TdApi.MessageText) msg.content;
+        TdApi.MessageText text = (TdApi.MessageText) nativeMessage.content;
         this.text=text.text.text;
+        this.nativeMessage = nativeMessage;
     }
 }
